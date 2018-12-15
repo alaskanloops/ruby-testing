69639078ae020995424d123dc932619f4bcfb247
Johannes C. Meyer <jmeyer1@gci.com>
2018-12-15 12:48:29 -0900

    Remove Playground code

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/bowling_game'
 
 describe BowlingGame do
     let(:game) { BowlingGame.new }
+
     it 'scores a gutter game' do
         game.pins([0] * 20)
         expect(game.score).to eq(0)
@@ -11,4 +12,9 @@ describe BowlingGame do
         game.pins([1] * 20)
         expect(game.score).to eq(20) 
     end
+
+    # it 'scores a game with spare' do
+    #     game.pins([4,6,5] + [0]*17)
+    #     expect(game.score).to eq(20)
+    # end
 end
