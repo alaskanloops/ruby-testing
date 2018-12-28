359afc4d4a7daddcf80e8484ae9949b76ca8cddb
Johannes C. Meyer <jmeyer1@gci.com>
2018-12-27 21:48:19 -0900

    Complete bowlign game kata

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -13,8 +13,18 @@ describe BowlingGame do
         expect(game.score).to eq(20) 
     end
 
-    # it 'scores a game with spare' do
-    #     game.pins([4,6,5] + [0]*17)
-    #     expect(game.score).to eq(20)
-    # end
+    it 'scores a game with spare' do
+        game.pins([4,6,5] + [0]*17)
+        expect(game.score).to eq(20)
+    end
+
+    it 'scores a game with strike' do
+        game.pins([10,3,5] + [0]*17)
+        expect(game.score).to eq(26)
+    end
+
+    it 'scores a perfect game' do
+        game.pins([10]*12)
+        expect(game.score).to eq(300)
+    end
 end
